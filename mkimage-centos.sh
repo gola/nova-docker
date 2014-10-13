47d266caf82cc39d06f14f7b80c6f48a551fd4af
gukai <307475423@qq.com>
2014-10-14 06:27:09 +0800

    first version.

--- mkimage-centos.sh
+++ mkimage-centos.sh
@@ -12,16 +12,25 @@ $(basename $0) [OPTIONS] <name>
 OPTIONS:
   -y <yumconf>  The path to the yum config to install packages from. The
                 default is /etc/yum.conf.
+  -p <pckg_list>  The packages install in the tempdir.
+  -g <group_list>  The group install in the tempdir(not used now).
 EOOPTS
     exit 1
 }
 
 # option defaults
 yum_config=/etc/yum.conf
-while getopts ":y:h" opt; do
+while getopts ":y:p:g:h" opt; do
     case $opt in
         y)
             yum_config=$OPTARG
+            ;;
+        p)
+            pckg_list=$OPTARG
+            ;;
+        g)
+            #not used now.
+            group_list=$OPTARG
             ;;
         h)
             usage
@@ -39,6 +48,14 @@ if [[ -z $name ]]; then
     usage
 fi
 
+if [ -z "$pckg_list" ]; then
+    echo "Package list is empty, It will install Core_Group only."
+    echo "set -p option specify the package list file."
+fi
+
+#-------------------
+#echo "pckg list is  $pckg_list"
+#exit 0
 #--------------------
 
 target=$(mktemp -d --tmpdir $(basename $0).XXXXXX)
@@ -57,9 +74,24 @@ mknod -m 666 "$target"/dev/tty0 c 4 0
 mknod -m 666 "$target"/dev/urandom c 1 9
 mknod -m 666 "$target"/dev/zero c 1 5
 
+
+# install centos-release first.
+rpm -qa | grep yum-utils
+ret=$?
+if [ ret != 0 ]; then
+    yum install -y yum-utils
+fi
+yumdownloader centos-release
+rpm -ivh --root="$target" ./centos-release*.rpm
+
+# install Core_group and your package list.
 yum -c "$yum_config" --installroot="$target" --setopt=tsflags=nodocs \
     --setopt=group_package_types=mandatory -y groupinstall Core
+if [ -n "$pckg_list" ];then
+    yum -c "$yum_config" --installroot="$target" install -y $pckg_list
+fi
 yum -c "$yum_config" --installroot="$target" -y clean all
+
 
 cat > "$target"/etc/sysconfig/network <<EOF
 NETWORKING=yes
