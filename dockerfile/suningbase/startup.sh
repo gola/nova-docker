3206701655fa7a8f334e93f6e39e49b1bdd9ca82
gukai <307475423@qq.com>
2015-04-17 13:40:07 +0800

    docker启动日志/ docker添加默认启动配置目录/ docker重新安装解决passwd失效的包.

--- dockerfile/suningbase/startup.sh
+++ dockerfile/suningbase/startup.sh
@@ -5,25 +5,25 @@ sysctl_file="/etc/sysctl.conf"
 docker_conf_dir="/etc/openstack-docker"
 log_file="/var/log/startup.log"
 
-echo `date` " Start to startup the docker container." > $log_file
+echo `date` " Start to startup the docker container." >> $log_file
 
 if [ ! -d $docker_conf_dir ]; then
-    echo `date` " ERROR: $docker_conf_dir is not exist." > $log_file
+    echo `date` " ERROR: $docker_conf_dir is not exist." >> $log_file
 fi
 
 if [ -f ${docker_conf_dir}/hosts ];then
     cat ${docker_conf_dir}/hosts > ${hosts_file}
-    echo `date` " reset hosts to $hosts_file" > $log_file
+    echo `date` " reset hosts to $hosts_file" >> $log_file
 fi
 
 if [ -f ${docker_conf_dir}/resolv.conf ];then
     cat ${docker_conf_dir}/resolv.conf > ${resolv_file}
-    echo `date` " reset resolv.conf to $resolv_file" > $log_file
+    echo `date` " reset resolv.conf to $resolv_file" >> $log_file
 fi
 
 if [ -f ${docker_conf_dir}/sysctl.conf ];then
     cat ${docker_conf_dir}/sysctl.conf > ${sysctl_file}
-    echo `date` " reset sysctl to $sysctl_file" > $log_file
+    echo `date` " reset sysctl to $sysctl_file" >> $log_file
 fi
 
-echo `date` " Finish start Docker contianer.:)" > $log_file
+echo `date` " Finish start Docker contianer.:)" >> $log_file
