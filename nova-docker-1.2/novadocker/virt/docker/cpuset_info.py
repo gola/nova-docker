6afb1c98df68a46d0c464c4e51ba53caafe3a25b
gukai <307475423@qq.com>
2015-07-23 20:56:04 +0800

    nova docker cpuset mode feature.

---
+++ nova-docker-1.2/novadocker/virt/docker/cpuset_info.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+from nova import utils
+from novadocker.virt.docker import hostinfo
+
+#def get_cpu_info():
+#    with open('/proc/cpuinfo') as f:
+#       pcpu_total = 0
+#       m = f.read().split()
+#       for i in range(len(m)):
+#           if m[i] == 'processor':
+#               pcpu_total = pcpu_total + 1
+#    return pcpu_total if pcpu_total > 1 else 1
+
+
+class CpusetStatsMap(object):
+    """ get cpuset stats """
+
+    def __init__(self):
+        self.container_list=[]
+        self.cpu_num = hostinfo.get_cpu_info()
+        self.cpu_map = {}
+        for i in range(self.cpu_num):
+            cpu_name = "cpu" + str(i)
+            self.cpu_map[cpu_name] = 0
+
+    def _get_container_list(self, all=False):
+        if all:
+            self.container_list = utils.execute('docker', 'ps', '--all', '-q', '--no-trunc')[0].split('\n')
+        else:
+            self.container_list = utils.execute('docker', 'ps', '-q', '--no-trunc')[0].split('\n') 
+        self.container_list.remove('')
+
+    def _get_cpuset_str(self, container_id):
+         cgroup_path = "docker/" + str(container_id)
+         cpuset_str = utils.execute('cgget', '-v', '-r', 'cpuset.cpus', cgroup_path)[0].split('\n')[1]
+         return cpuset_str
+
+    def _push_in_map(self, cpu_list):
+        for cpu in cpu_list:
+            cpu_key = "cpu" + cpu
+            self.cpu_map[cpu_key] = self.cpu_map[cpu_key] + 1
+
+    def get_map(self):
+        self._get_container_list(False)
+        for id in self.container_list:
+            cn_cpu_str = self._get_cpuset_str(id)
+            cn_cpu_list = parse_cpuset()            
+            cn_cpu_list.parse(cn_cpu_str)
+            self._push_in_map(cn_cpu_list.get_cpu_list())
+        return self.cpu_map
+       
+    def less_set_cpus(self, num):
+        ret_cpus = []
+        less_set_list = sorted(self.cpu_map.items(), key=lambda d:d[1], reverse=False)[0:num]
+        for cpu in less_set_list:
+            ret_cpus.append(cpu[0])
+
+        return ret_cpus
+
+    def print_info(self):
+        #print self.container_list
+        print self.cpu_map
+
+
+
+class parse_cpuset(object):
+    """parse cpuset config to a list"""
+
+    def __init__(self):
+        self.first_num = ''
+        self.sec_num = ''
+        self.sec_mark = False
+        self.cpu_list = []
+
+    def _cl_swap_info(self):
+        self.first_num = ''
+        self.sec_num = ''
+        self.sec_mark = False
+
+    def _append_cpu_list(self):
+        if self.sec_mark:
+            for i in range(int(self.first_num), int(self.sec_num)+1):
+                self.cpu_list.append(str(i))
+            self._cl_swap_info()
+        else:
+            self.cpu_list.append(self.first_num)
+            self._cl_swap_info()
+
+    def parse(self, cpuset_str):
+        self._cl_swap_info()
+        for chr in cpuset_str:
+            if chr.isdigit():
+                if self.sec_mark:
+                    self.sec_num = self.sec_num + chr
+                else:
+                    self.first_num = self.first_num + chr
+            elif chr == "-":
+                self.sec_mark = True
+            elif chr ==",":
+                self._append_cpu_list()
+
+        self._append_cpu_list()
+
+    def get_cpu_list(self):
+        return self.cpu_list
+
+if __name__ == '__main__':
+    cpustats = CpusetStatsMap()
+    cpustats.get_map()
+    cpustats.print_info()
+    print cpustats.less_set_cpus(2)
+
+
--- nova-docker-1.2/novadocker/virt/docker/driver.py
+++ nova-docker-1.2/novadocker/virt/docker/driver.py
@@ -40,6 +40,7 @@ from nova.virt import driver
 from nova.virt import images
 from novadocker.virt.docker import client as docker_client
 from novadocker.virt.docker import hostinfo
+from novadocker.virt.docker import cpuset_info
 from novadocker.virt.docker import network
 from novadocker.virt import hostutils
 
@@ -56,6 +57,10 @@ docker_opts = [
                     'snapshots.'),
     cfg.StrOpt('docker_allocation_ratio',
                default=5)
+    cfg.StrOpt('docker_cpu_mode',
+               default='cpushare',
+               help='Three mode support: cpushare(default)/cpuset/mix,'
+                    'refer man of docker-run to definition of cpushare and cpuset ')
 ]
 
 CONF.register_opts(docker_opts, 'docker')
@@ -303,6 +308,7 @@ class DockerDriver(driver.ComputeDriver):
             'Image': image_name,
             'Memory': self._get_memory_limit_bytes(instance),
             'CpuShares': self._get_cpu_shares(instance),
+            'Cpuset': self._get_cpu_set(instance),
             'NetworkDisabled': True,
         }
 
@@ -496,8 +502,26 @@ class DockerDriver(driver.ComputeDriver):
         the user (e.g. docker registry) which has
         the default CpuShares value of zero.
         """
-        flavor = flavors.extract_flavor(instance)
-        return int(flavor['vcpus']) * 1024
+        cpu_mode = CONF.docker.docker_cpu_mode
+        if cpu_mode == 'cpuset' or 'mix':
+            flavor = flavors.extract_flavor(instance)
+            return int(flavor['vcpus']) * 1024
+        else:
+            return
+
+    def _get_cpu_set(self, instance):
+        cpu_mode = CONF.docker.docker_cpu_mode
+        if cpu_mode == 'cpuset' or 'mix':
+            flavor = flavors.extract_flavor(instance)
+            cpu_num = int(flavor['vcpus'])
+            cpustats = cpuset_info.CpusetStatsMap()
+            cpustats.get_map()
+            ori_list = cpustats.less_set_cpus(cpu_num)
+            for cpu in ori_list:
+                set_str = set_str  +  ',' +  cpu[3:]
+            return set_str
+        else:
+            return
 
     def _create_container(self, instance, args):
         name = instance['name']
