cc8e606b72687cd92612927d79d78a51e00a0609
gukai <307475423@qq.com>
2015-07-28 10:41:59 +0800

    [release] [merge from master]Feautre: obligate cpu for system by cpuset.

--- nova-docker-1.2/novadocker/virt/docker/cpuset_info.py
+++ nova-docker-1.2/novadocker/virt/docker/cpuset_info.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 from nova import utils
+from nova.openstack.common import log
 from novadocker.virt.docker import hostinfo
 
+LOG = log.getLogger(__name__)
 #def get_cpu_info():
 #    with open('/proc/cpuinfo') as f:
 #       pcpu_total = 0
@@ -15,19 +17,37 @@ from novadocker.virt.docker import hostinfo
 class CpusetStatsMap(object):
     """ get cpuset stats """
 
-    def __init__(self):
+    def __init__(self, system_cpuset):
         self.container_list=[]
         self.cpu_num = hostinfo.get_cpu_info()
         self.cpu_map = {}
+        self.sys_cpuset_list = []
+        if system_cpuset != '-1':
+            self.sys_cpuset_list = self._get_system_cpuset(system_cpuset)
         for i in range(self.cpu_num):
             cpu_name = "cpu" + str(i)
-            self.cpu_map[cpu_name] = 0
+            if str(i) in self.sys_cpuset_list:
+                self.cpu_map[cpu_name] = -1
+            else:
+                self.cpu_map[cpu_name] = 0
+
+    def _get_system_cpuset(self, system_cpuset):
+        sys_cpu_list = ParseCpuset()
+        sys_cpu_list.parse(system_cpuset)
+        return sys_cpu_list.get_cpu_list()
+
+    def get_unsystem_cpu(self):
+        unsystem_list=[]
+        for cpu in self.cpu_map:
+            if self.cpu_map[cpu] != -1:
+                unsystem_list.append(cpu)
+        return unsystem_list
 
     def _get_container_list(self, all=False):
         if all:
             self.container_list = utils.execute('docker', 'ps', '--all', '-q', '--no-trunc')[0].split('\n')
         else:
-            self.container_list = utils.execute('docker', 'ps', '-q', '--no-trunc')[0].split('\n') 
+            self.container_list = utils.execute('docker', 'ps', '-q', '--no-trunc')[0].split('\n')
         self.container_list.remove('')
 
     def _get_cpuset_str(self, container_id):
@@ -37,6 +57,7 @@ class CpusetStatsMap(object):
 
     def _push_in_map(self, cpu_list):
         for cpu in cpu_list:
+            if cpu == '0': continue
             cpu_key = "cpu" + cpu
             self.cpu_map[cpu_key] = self.cpu_map[cpu_key] + 1
 
@@ -44,16 +65,23 @@ class CpusetStatsMap(object):
         self._get_container_list(False)
         for id in self.container_list:
             cn_cpu_str = self._get_cpuset_str(id)
-            cn_cpu_list = parse_cpuset()            
+            cn_cpu_list = ParseCpuset()
             cn_cpu_list.parse(cn_cpu_str)
             self._push_in_map(cn_cpu_list.get_cpu_list())
         return self.cpu_map
-       
+
     def less_set_cpus(self, num):
+        cont = 0
         ret_cpus = []
-        less_set_list = sorted(self.cpu_map.items(), key=lambda d:d[1], reverse=False)[0:num]
+        less_set_list = sorted(self.cpu_map.items(), key=lambda d:d[1], reverse=False)
         for cpu in less_set_list:
-            ret_cpus.append(cpu[0])
+            if not cpu[0][3:] in self.sys_cpuset_list:
+                ret_cpus.append(cpu[0])
+                cont = cont + 1
+            else:
+                continue
+            if cont >= num:
+                break
 
         return ret_cpus
 
@@ -62,8 +90,7 @@ class CpusetStatsMap(object):
         print self.cpu_map
 
 
-
-class parse_cpuset(object):
+class ParseCpuset(object):
     """parse cpuset config to a list"""
 
     def __init__(self):
@@ -109,5 +136,3 @@ if __name__ == '__main__':
     cpustats.get_map()
     cpustats.print_info()
     print cpustats.less_set_cpus(2)
-
-
